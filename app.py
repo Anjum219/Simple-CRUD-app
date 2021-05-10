e07e20262ee497d195617fbbea85194435b293ba
Anjum219 <anjumhaz@gmail.com>
2021-05-10 13:50:40 +0600

    Set up MYSQL cursor connection [getting error]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
   
 app = Flask(__name__)
@@ -6,14 +6,31 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'simple_app'
  
 mysql = MySQL(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/') 
+@app.route('/enter', methods = ['POST', 'GET'])
+def enter():
+    if request.method == 'GET':
+        return render_template('form.html')
+     
+    if request.method == 'POST':
+        userDetails = request.form
+        first_name = userDetails['first_name']
+        last_name = userDetails['last_name']
+        email = userDetails['email']
+        mobile_no = userDetails['mobile_no']
+        cursor = mysql.get_db().cursor()
+        cursor.execute(''' INSERT INTO users VALUES(%s, %s, %s, %s)''', (first_name, last_name, email, mobile_no))
+        mysql.connection.commit()
+        cursor.close()
+        return "Done!!"
 
 if __name__ == '__main__':
-  
-    app.run()
+    app.run(host = 'localhost', port = 8000)
